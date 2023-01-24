75f2e122a174f3e7a2bc7fc0050abba674dc4b8e
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-24 10:15:21 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -2,5 +2,8 @@ import streamlit
 
 streamlit.title ('My Parent New Healthy Diner')
 streamlit.header ('Breakefast Menu')
-
+streamlit.text ('Allu Paratha')
+streamlit.text ('Anda Paratha')
+streamlit.text ('Qeema Paratha')
+streamlit.text ('Doodh Pati')
 
