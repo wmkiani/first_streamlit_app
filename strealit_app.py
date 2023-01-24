beab93afae868809acf6a3a51fed56323530f087
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-24 10:25:22 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -10,4 +10,6 @@ streamlit.text ('🥛☕ Doodh Pati 🥛☕')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
