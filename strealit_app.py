a98aafbbd27aa1d7ad7b7980ebbe771d39319597
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-24 12:16:21 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -14,16 +14,17 @@ streamlit.markdown("<p style='text-align: center; color: Blue'>'🥞 Lebanese Br
 #streamlit.text ('🍖 Qeema Paratha 🍖')
 streamlit.markdown("<p style='text-align: center; color: Blue'>'🥛☕ Doodh Pati 🥛☕'</p>", unsafe_allow_html=True)
 #streamlit.text ('🥛☕ Doodh Pati 🥛☕')
-streamlit.markdown("<h2 style='text-align: center; color: Purple'>🍌🥭 Build Your Own Fruit Smoothie 🥝🍇</h1>", unsafe_allow_html=True)
+streamlit.markdown("<h2 style='text-align: center; color: Purple'>🍌🥭 Build Your Own Fruit Smoothie 🥝🍇</h2>", unsafe_allow_html=True)
 #streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index ('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 
 # Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = streamlit.multiselect("pick some fruits.", list(my_fruit_list.index), ['Avocado','Strawberries'])
+pick_some=streamlit.markdown("<h2 style='text-align: center; color: Purple'>Pick some fruits 🍌🥭🥝🍇:</h2>", unsafe_allow_html=True)
+fruits_selected = streamlit.multiselect([pick_some], list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page streamlit.dataframe(fruits_to_show)
 streamlit.dataframe(fruits_to_show)
