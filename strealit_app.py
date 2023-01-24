52d09c83061e965f1a31996b8fdfa56988047a2a
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-24 10:23:27 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title ('My Parent New Healthy Diner')
 streamlit.header ('🍽 Breakefast Menu 🍽')
@@ -8,3 +9,5 @@ streamlit.text ('🍖 Qeema Paratha 🍖')
 streamlit.text ('🥛☕ Doodh Pati 🥛☕')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
