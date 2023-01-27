b6fafad8f740fbedc15ac3c4a7a6e5c92a234003
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-27 10:42:20 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -31,4 +31,4 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.jason())
+streamlit.text(fruityvice_response.json())
