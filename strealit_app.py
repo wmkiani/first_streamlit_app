efe421944f7172f673e9abee173c579f3f0152e5
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-30 12:18:27 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -46,6 +46,6 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The Fruit list contais:")
-streamlit.text(my_data_rows)
+streamlit.dataframe(my_data_rows)
 
 
