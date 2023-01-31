e495194d351513daf65423cd988db6bf38a1122a
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-31 13:02:42 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -52,6 +52,7 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load List '):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
 
