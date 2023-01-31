1e95a51071ac604c9dc5e3a02329c6f99f8ece13
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-31 12:52:52 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -69,7 +69,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like add?')
 if streamlit.button('Get fruit to the List '):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
-   streamlit.dataframe(back_from_function)
+   streamlit.text(back_from_function)
         
         
         
