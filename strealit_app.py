dc61606e4726576c8dc19d8c95d264c31c395b82
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-31 10:58:28 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -45,7 +45,7 @@ try:
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # write your own comment - what does this do?
       streamlit.dataframe(fruityvice_normalized)
-  except URLError as e:
+except URLError as e:
     streamlit.error()
     
 #Snowflake connector
