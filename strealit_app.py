65a211a6aea442bfd49dfee9f96eee4a65876d11
wmkiani <123247446+wmkiani@users.noreply.github.com>
2023-01-31 10:57:12 +0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -38,7 +38,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
-   else:
+  else:
       #streamlit.write('The user entered :', fruit_choice)
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
       # write your own comment -what does the next line do? 
